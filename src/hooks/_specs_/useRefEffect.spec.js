bc0ce69123034e50f59eac2d28cf714ef83201f0
Jeremy Neander <jeremy.neander@gmail.com>
2022-12-09 12:57:49 -0600

    test: remove `only` call to run all tests

--- src/hooks/_specs_/useRefEffect.spec.js
+++ src/hooks/_specs_/useRefEffect.spec.js
@@ -4,7 +4,7 @@ import sinon from 'sinon'
 
 import {useRefEffect} from '..'
 
-describe.only('.useRefEffect()', () => {
+describe('.useRefEffect()', () => {
   let component
   let callbackFn
   let callbackTeardownFn
